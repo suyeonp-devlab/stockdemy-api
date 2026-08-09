d51c7b75edfb2731d1cc7ca6e7b91ffb96d604fd
suyeon <suyeondevlab@gmail.com>
2026-08-09 23:33:16 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/stockdemy/StockdemyApiApplication.java
@@ -0,0 +1,13 @@
+package com.stockdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockdemyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockdemyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockdemy/StockdemyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockdemyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
